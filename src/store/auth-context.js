b317d7dfd5cfb786eeb49102cec3eb143471c519
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-21 22:28:29 +0300

    log user out after time expired

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -7,20 +7,33 @@ const AuthContext = React.createContext({
   logout: () => {},
 });
 
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  const adjustedExpirationTime = new Date(expirationTime).getTime();
+
+  const remainingDuration = adjustedExpirationTime - currentTime;
+
+  return remainingDuration;
+};
+
 export const AuthContextProvider = (props) => {
   const initialToken = localStorage.getItem('token');
   const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
 
-  const loginHandler = (token) => {
-    setToken(token);
-    localStorage.setItem('token', token);
-  };
-
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem('token');
+  };
+
+  const loginHandler = (token, expirationTime) => {
+    setToken(token);
+    localStorage.setItem('token', token);
+
+    const remainingTime = calculateRemainingTime(expirationTime);
+
+    setTimeout(logoutHandler, remainingTime);
   };
 
   const contextValue = {
